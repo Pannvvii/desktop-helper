0a197df17369567c0a6a3b341c242557c9ed54cf
Pierre Allard <palla036@uottawa.ca>
2025-10-22 17:07:51 -0400

    Helper Code

--- DesktopHelper/HelperPrograms/Running/Helper.cs
+++ DesktopHelper/HelperPrograms/Running/Helper.cs
@@ -11,6 +11,11 @@ using System.Windows.Input;
 using DesktopHelper.ViewModels;
 using System.IO;
 using System.Reflection;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media.Animation;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Media;
 
 namespace DesktopHelper
 {
@@ -19,14 +24,45 @@ namespace DesktopHelper
         public static void Render(PaintEventArgs e)
         {
             Graphics graphics = e.Graphics;
+            //int heightWindow = (System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height);
+               // SystemParameters.FullPrimaryScreenHeight);
+            //int widthWindow = (int)Math.Floor(SystemParameters.FullPrimaryScreenWidth);
 
             //TODO:
             //Update point position every tick to move on a set path, change image to one of multiple for animation.
 
-            Point position = System.Windows.Forms.Control.MousePosition; // TEMPORARY, sets helper to stay on mouse cursor for testing
+
+            //Screen.PrimaryScreen.WorkingArea.height - Screen.PrimaryScreen.Bounds
+            int PSBH = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+            int TaskBarHeight = PSBH - System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height;
+
+            System.Drawing.Point position = new System.Drawing.Point(MainHelper.widthWindow-200, MainHelper.heightWindow-195);
+
+
+
+
+            var TaskLWeek = new List<TaskItem> {};
+            var TaskLToday = new List<TaskItem> {};
+            var storTask = new TaskItem { TaskName = "New Task", DueDate = null, HasReminder = false };
+            
+
+            //System.Drawing.Point position = System.Windows.Forms.Control.MousePosition; // TEMPORARY, sets helper to stay on mouse cursor for testing
             int width = 200;
             int height = 200; // Size of PNG for helper
-            String drawString = "No Tasks";
+            //String drawString = "No Tasks";
+            //Debug.WriteLine(Time.time);
+            int AppTime = (int)Math.Floor(Time.time);
+
+            // Create font and brush.
+            Font drawFont = new Font("Comic Sans MS", 16);
+            SolidBrush drawBrush = new SolidBrush(System.Drawing.Color.Blue);
+
+            // Set format    of string.
+            StringFormat drawFormat = new StringFormat();
+            drawFormat.FormatFlags = StringFormatFlags.NoWrap;
+
+            DateTime dateNow = DateTime.Now;
+            DateTime currTaskDate = DateTime.MinValue;
 
             //TODO:
             //Create two lists: Tasks due today, and tasks due this week
@@ -35,25 +71,232 @@ namespace DesktopHelper
             //When Task time (from both lists) - currtime = certain set of values, change notification event int to the right number
             //Check notification event number, if not zero display reminder about task
 
-            if (MainViewModel._tasks != null)   //Gets Task list, currently sets text output of helper to the last listed task
+            string reminderTextDay = "You Have the following tasks due today: ";
+            string reminderTextWeek = "You Have the following tasks due this week: ";
+            
+            //drawString = currTask.TaskName;
+            if (MainViewModel._tasks != null && dateNow != null && MainHelper.petTime != AppTime)   //Gets Task list, currently sets text output of helper to the last listed task
             {
-                foreach (var task in MainViewModel._tasks)
+                foreach (var currTask in MainViewModel._tasks)
                 {
-                    drawString = task.TaskName;
+                    currTaskDate = currTask.DueDate.Value;
+                    //if (currTask.DueDate >= DateTime.Now)
+                    if (true)
+                    {
+                        Debug.WriteLine("Date of Task: " + currTaskDate.Date);
+                        Debug.WriteLine("Date of Today: " + DateTime.Today);
+                        if (currTaskDate.Date == DateTime.Today)
+                        //if (true)
+                        {
+                            TaskLToday.Add(currTask);
+                            Debug.WriteLine("Task Added For Today: " + currTask.TaskName);
+                        } else if (dateNow.Subtract(currTaskDate).TotalDays <= 7)
+                        { 
+                            TaskLWeek.Add(currTask);
+                            Debug.WriteLine("Task Added For This Week: " + currTask.TaskName);
+                        }
+                    }
+                }
+
+                if (TaskLToday.Any())
+                {
+                    foreach (var notifTask in TaskLToday)
+                    {
+                        reminderTextDay = reminderTextDay + notifTask.TaskName + ", ";
+                    }
+                    reminderTextDay = reminderTextDay.Remove(reminderTextDay.Length - 2);
+                }
+
+                if (TaskLWeek.Any())
+                {
+                    foreach (var notifTask in TaskLWeek)
+                    {
+                        reminderTextWeek = reminderTextWeek + notifTask.TaskName + ", ";
+                    }
+                    reminderTextWeek = reminderTextWeek.Remove(reminderTextWeek.Length - 2);
+                }
+
+                
+
+            }else if (MainViewModel._tasks == null)
+            {
+                MainHelper.drawString = "No Tasks!";
+                MainHelper.isbubble = false;
+            }
+
+
+            //petx pety petStatus petTime petMoveDist
+            //bubblex bubbley isbubble
+            //petStatus
+            Random rnd = new Random();
+            int rndResult = 0;
+            Random rndsec = new Random();
+            int rndDist = 0;
+            Random rndthird = new Random();
+            int rndDirect = 0;
+
+            int speed = 50;
+
+            //(AppTime % 2 == 0) && 
+            //PET UPDATE
+
+
+            if (MainHelper.petTime != AppTime)
+            {
+                MainHelper.petTime = AppTime;
+                Debug.WriteLine("Anim: "+MainHelper.petAnimStage);
+                Debug.WriteLine("NotifFlag: " + MainHelper.needNotifFlag);
+                Debug.WriteLine("Notif Length: "+MainHelper.notifLength);
+
+                if (MainHelper.needNotifFlag == 0 && MainHelper.petStatus == "Idle")
+                {
+                    //Just booted intro
+                    if (MainHelper.notifLength != 0)
+                    {
+                        MainHelper.isbubble = true;
+                        if (MainHelper.notifLength > 2)
+                        {
+                            MainHelper.drawString = reminderTextDay;
+                        }else if (MainHelper.notifLength <= 2)
+                        {
+                            MainHelper.drawString = reminderTextWeek;
+                        }
+                        if (MainHelper.notifLength > 0)
+                        {
+                            MainHelper.notifLength--;
+                        }
+                    }
+                    else if (MainHelper.notifLength == 0)
+                    {
+                        MainHelper.needNotifFlag = 1;
+                        MainHelper.notifLength = 5;
+                    }
+                }
+                else if (MainHelper.needNotifFlag == 2 && MainHelper.petStatus == "Idle")
+                {
+                    //Grab attention notification
+                    if (MainHelper.notifLength != 0)
+                    {
+                        MainHelper.isbubble = true;
+                        if (MainHelper.notifLength > 0)
+                        {
+                            MainHelper.drawString = "Time for a Reminder!";
+                        }
+                        if (MainHelper.notifLength > 0)
+                        {
+                            MainHelper.notifLength--;
+                        }
+                    }
+                    else if (MainHelper.notifLength == 0)
+                    {
+                        MainHelper.needNotifFlag = 1;
+                        MainHelper.notifLength = 5;
+                    }
+                }
+                else if (MainHelper.needNotifFlag == 1)
+                {
+                    MainHelper.isbubble = false;
+                    if (AppTime % 20 == 0)
+                    {
+                        MainHelper.needNotifFlag = 2;
+                    }
+                }
+
+
+                if ((MainHelper.petStatus == "Idle") && (MainHelper.needNotifFlag == 1))
+                {
+                    rndResult = rnd.Next(1, 14);
+                    Debug.WriteLine("RandRes: " + rndResult);
+                    if (rndResult == 6)
+                    {
+                        rndDist = rndsec.Next(1, 8);
+                        MainHelper.petMoveDist = rndDist;
+                        rndDirect = rndthird.Next(0, 3);
+                        MainHelper.petMoveDirection = rndDirect;
+                        MainHelper.petStatus = "Moving";
+                        MainHelper.petAnimStage = 5;
+                        Debug.WriteLine(MainHelper.petStatus);
+                        Debug.WriteLine("Direction: " + MainHelper.petMoveDirection);
+                    }
+                    else
+                    {
+                        if (MainHelper.petAnimStage == 4 || MainHelper.petAnimStage > 4)
+                        {
+                            MainHelper.petAnimStage = 1;
+                        }
+                        else
+                        {
+                            MainHelper.petAnimStage++;
+                        }
+                    }
+                }
+                else if (MainHelper.petStatus == "Moving")
+                {
+                    if (MainHelper.petMoveDirection == 1)
+                    {
+                        if (MainHelper.bubblex - speed >= 0)
+                        {
+                            MainHelper.bubblex = MainHelper.bubblex - speed;
+                            MainHelper.petx = MainHelper.petx - speed;
+                            if (MainHelper.petAnimStage == 8 || MainHelper.petAnimStage < 4)
+                            {
+                                MainHelper.petAnimStage = 5;
+                            }
+                            else
+                            {
+                                MainHelper.petAnimStage++;
+                            }
+
+                            MainHelper.petMoveDist = MainHelper.petMoveDist - 1;
+                            if (MainHelper.petMoveDist == 0)
+                            {
+                                MainHelper.petStatus = "Idle";
+                                MainHelper.petAnimStage = 1;
+                                Debug.WriteLine(MainHelper.petStatus);
+                            }
+                        }
+                        else
+                        {
+                            MainHelper.petStatus = "Idle";
+                            MainHelper.petAnimStage = 1;
+                        }
+                    }
+                    else if (MainHelper.petMoveDirection == 2)
+                    {
+                        if ((MainHelper.petx + (200 + speed)) <= MainHelper.widthWindow)
+                        {
+                            MainHelper.bubblex = MainHelper.bubblex + speed;
+                            MainHelper.petx = MainHelper.petx + speed;
+                            if (MainHelper.petAnimStage == 8 || MainHelper.petAnimStage < 4)
+                            {
+                                MainHelper.petAnimStage = 4;
+                            }
+                            else
+                            {
+                                MainHelper.petAnimStage++;
+                            }
+
+                            MainHelper.petMoveDist = MainHelper.petMoveDist - 1;
+                            if (MainHelper.petMoveDist == 0)
+                            {
+                                MainHelper.petStatus = "Idle";
+                                MainHelper.petAnimStage = 1;
+                                Debug.WriteLine(MainHelper.petStatus);
+                            }
+                        }
+                        else
+                        {
+                            MainHelper.petStatus = "Idle";
+                            MainHelper.petAnimStage = 1;
+                            Debug.WriteLine(MainHelper.petStatus);
+                        }
+                    }
                 }
             }
 
-            // Create font and brush.
-            Font drawFont = new Font("Arial", 16);
-            SolidBrush drawBrush = new SolidBrush(Color.Blue);
-
-            // Set format of string.
-            StringFormat drawFormat = new StringFormat();
-            drawFormat.FormatFlags = StringFormatFlags.NoWrap;
-
             //Get PNG for helper, Future versions will swap between multiple for animations.
             Assembly myAssembly = Assembly.GetExecutingAssembly();
-            Stream myStream = myAssembly.GetManifestResourceStream("DesktopHelper.HelperPrograms.Running.Resources.Image1.png");
+            Stream myStream = myAssembly.GetManifestResourceStream("DesktopHelper.HelperPrograms.Running.Resources.Cat.png");
             Bitmap bmp = new Bitmap(myStream);
             System.Drawing.Image sourceImage = bmp;
 
@@ -62,8 +305,47 @@ namespace DesktopHelper
             //Check if helper is disabled before drawing to the screen
             if (MainHelper.HelperEnable == true)
             {
+                position = new System.Drawing.Point(MainHelper.petx, MainHelper.pety);
+                myStream = myAssembly.GetManifestResourceStream("DesktopHelper.HelperPrograms.Running.Resources.Cat.png");
+                bmp = new Bitmap(myStream);
+                sourceImage = bmp;
                 graphics.DrawImage(sourceImage, position.X, position.Y, sourceRect, GraphicsUnit.Pixel);
-                graphics.DrawString(drawString, drawFont, drawBrush, position.X + 60, position.Y + 85, drawFormat);
+
+
+                if (MainHelper.isbubble == true)
+                {
+                    position = new System.Drawing.Point(MainHelper.bubblex, MainHelper.bubbley);
+                    //myStream = myAssembly.GetManifestResourceStream("DesktopHelper.HelperPrograms.Running.Resources.Image1.png");
+                    //bmp = new Bitmap(myStream);
+                    //sourceImage = bmp;
+                    //graphics.DrawImage(sourceImage, position.X, position.Y, sourceRect, GraphicsUnit.Pixel);
+
+                    float padding = 4f;
+                    float maxWidth = 300f;
+                    float fixedBottomY = position.Y;
+                    //SizeF textSize = graphics.MeasureString(MainHelper.drawString, drawFont, (int)maxWidth);
+
+                    StringFormat format = new StringFormat();
+                    format.Alignment = StringAlignment.Near;
+                    format.LineAlignment = StringAlignment.Near;
+                    format.FormatFlags = StringFormatFlags.LineLimit;
+                    format.Trimming = StringTrimming.Word;
+
+                    RectangleF layoutRect = new RectangleF(0, 0, maxWidth, float.MaxValue);
+
+                    SizeF measuredSize = graphics.MeasureString(MainHelper.drawString, drawFont, new SizeF(maxWidth, float.MaxValue), format);
+                    float totalTextHeight = measuredSize.Height;
+                    float originY = fixedBottomY - totalTextHeight;
+
+                    RectangleF rect = new RectangleF(position.X - padding - 55, originY, maxWidth + 2 * padding, measuredSize.Height + 2 * padding);
+                    graphics.FillRectangle(System.Drawing.Brushes.White, rect);
+                    graphics.DrawRectangle(Pens.White, rect.X, rect.Y, rect.Width, rect.Height);
+
+                    RectangleF textRect = new RectangleF(position.X - 55, originY+padding, maxWidth, measuredSize.Height);
+
+                    //graphics.DrawString(MainHelper.drawString, drawFont, drawBrush, position.X + 55, position.Y + 70, drawFormat);
+                    graphics.DrawString(MainHelper.drawString, drawFont, drawBrush, textRect, format);
+                }
             }
             else
             {
